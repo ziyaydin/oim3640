fc9feba4d83c770c109c78197e784369c02302d0
ziyaydin <zaydin1@babson.edu>
2022-01-20 17:20:09 -0500

    Create calc.py

---
+++ session02/calc.py
@@ -0,0 +1,20 @@
+# ex02-1
+# How many seconds are there in 42 minutes 42 seconds?
+
+print((60 * 42) + 42)
+print("there are 2562 seconds in 42 minutes and 42 seconds" )
+
+# ex02-2
+# How many miles are there in 10 kilometers? 
+print(10 / 1.62)
+print("there are 6.17 miles in 10 kilometers")
+
+#ex02-3
+#If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace (time per mile in minutes and seconds)? What is your average speed in miles per hour?
+print(2562 / 3600)
+print("Total running time is 0.711 hour")
+print(6.17 / 0.711)
+print("In one hour, I can run 8.68 miles. This means that my average speed is 8.68 mph")
+
+print((6.17 / 2562) * 60)
+print( "In one minute, I can run for 0.144 miles. This means that my pace is 0.144 miles/minute")
