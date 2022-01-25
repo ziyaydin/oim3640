8ece6532b8a4760b1b9fa279c228bb04117d1e09
ziyaydin <zaydin1@babson.edu>
2022-01-25 18:32:33 -0500

    Create hello.py

---
+++ session03/hello.py
@@ -0,0 +1,57 @@
+name = input("Please enter your name >> ")
+
+print('Hello,', name)
+
+n = 100
+
+print( n +200)
+
+m = n + 750
+
+a = "abc"
+b=a
+a = "XYZ"
+print(b)
+
+first_name = 'John '
+last_name = 'Lennon'
+print(first_name + last_name)
+
+print('Naah, na na nanana naah, nanana naah, hey Jude. \n' * 10)
+
+actor = 'Joaquin Phoenix'
+year = 2020
+movie = 'Joker'
+print(f'{actor} wins Best Actor for {movie} at Golden Globes {year}.')
+
+pi = 3.1415926
+print(f'Pi equals {pi:.5f}.')
+print(f'Pi equals {pi:.5}.')
+print(f'Pi equals {pi:8.5f}.')
+print(f'Pi equals {pi:8.2f}.')
+
+a = 2021
+
+# binary
+print(f'{a:b}')
+
+# hexadecimal
+print(f"{a:x}")
+
+# octal
+print(f"{a:o}")
+
+# scientific
+print(f"{a:e}")
+
+s1 = 'a'
+s2 = 'ab'
+s3 = 'abc'
+s4 = 'abcd'
+
+print(f'{s1:>10}')
+print(f'{s2:>10}')
+print(f'{s3:>10}')
+print(f'{s4:>10}')
+
+#end of session 3
