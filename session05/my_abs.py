b53b8fc16210724ca2fc8b30de2b6d159a17d8b8
ziyaydin <zaydin1@babson.edu>
2022-02-02 10:36:40 -0500

    Update my_abs.py

--- session05/my_abs.py
+++ session05/my_abs.py
@@ -49,16 +49,16 @@ print(my_abs_3(-508))
 
 def quadratic(a, b, c):
     """solves the quadratic equation, the format of ax^2 + bx + c = 0 """
-    distance_formula = b * b - 4 * a * c
-    sqrt_part = math.sqrt(abs(distance_formula))
-    if distance_formula > 0:
-        print((-b + sqrt_part) / (2 * a))
-        print((-b - sqrt_part) / (2 * a))
-    elif distance_formula == 0:
+    discriminant_of_polynomial = b * b - 4 * a * c
+    sqrt_part = math.sqrt(abs(discriminant_of_polynomial))
+    if discriminant_of_polynomial > 0:
+        print((-b + sqrt_part) / (2 * a),(-b - sqrt_part) / (2 * a) )
+    elif discriminant_of_polynomial == 0:
         print(-b / (2 * a))
     else:
-        print('Sorry, didin\'t learn this in highschool')
+        print('Sorry, I miss this class in highschool. I can\'t remember what happens when the discriminant of the polynomial is less than 0 :(')
 
 
 quadratic(1, 3, 4)
 quadratic(1, 3, -4)
+
