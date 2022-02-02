8901c5ed22d4452c83745e5678c98a4ac2db7eb4
ziyaydin <zaydin1@babson.edu>
2022-02-02 00:38:59 -0500

    Update my_abs.py

--- session05/my_abs.py
+++ session05/my_abs.py
@@ -1,4 +1,7 @@
-#ex_03
+# ex_03
+import math
+
+
 def my_abs(number):
     """print the absolute value of the given number"""
     if number < 0:
@@ -6,10 +9,13 @@ def my_abs(number):
     else:
         print(number)
 
+
 my_abs(-100)
 my_abs(100)
 
-#ex_04
+# ex_04
+
+
 def my_abs_2(number):
     """return the absolute value of the given number"""
     if number < 0:
@@ -17,36 +23,42 @@ def my_abs_2(number):
     else:
         return number
 
+
 x = my_abs_2(-80)
 print(my_abs_2(x))
 print(my_abs_2(-50))
 
-#ex_05
+# ex_05
+
+
 def my_abs_3(number):
     """if number is an integer or float take the absolute vallue, if it something else say BYE"""
-    if isinstance(number,int):
+    if isinstance(number, int):
         return my_abs_2(number)
-    elif isinstance(number,float):
+    elif isinstance(number, float):
         return my_abs_2(number)
     else:
         return 'Bye'
+
 
 print(my_abs_3('ziya'))
 print(my_abs_3(-508))
 
 # #ex_06
-import math  
+
+
 def quadratic(a, b, c):
     """solves the quadratic equation, the format of ax^2 + bx + c = 0 """
     distance_formula = b * b - 4 * a * c
     sqrt_part = math.sqrt(abs(distance_formula))
-    if distance_formula > 0:  
-        print((-b + sqrt_part) / (2 * a))  
-        print((-b - sqrt_part) / (2 * a))  
+    if distance_formula > 0:
+        print((-b + sqrt_part) / (2 * a))
+        print((-b - sqrt_part) / (2 * a))
     elif distance_formula == 0:
-        print(-b / (2 * a))  
+        print(-b / (2 * a))
     else:
         print('Sorry, didin\'t learn this in highschool')
+
 
 quadratic(1, 3, 4)
 quadratic(1, 3, -4)
