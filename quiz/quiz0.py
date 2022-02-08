2b5ae881cf3cc83eb80f883aacf103adffbf8edd
ziyaydin <zaydin1@babson.edu>
2022-02-08 16:29:45 -0500

    Update quiz0.py

--- quiz/quiz0.py
+++ quiz/quiz0.py
@@ -11,6 +11,9 @@ Notice:
 3. Write your own test code, i.e. call the function.
 """
 
+from time import monotonic
+
+
 def weight():
     """
     this function takes your weight on earth and converts it to your weight in moon
@@ -19,8 +22,39 @@ def weight():
     #1.Ask the weight of the perosn on earth
     #  Convert it to your weight in moon by multiplying with 16.5%
     #  Print the weight in moon  
-    weight = int(input('What is your weight on earth:'))
+    weight = float(input('What is your weight on earth:'))
     print('Your weight in moon is', weight * 0.165)
 
-weight()
+# weight()
 
+"""
+Question 2:
+Write a function that takes two arguments - 1. weight on earth (float), 2. planet (str) - and returns the equivalent weight on that planet. We assume Moon is a planet although it is not.
+Weight on Moon = weight on Earth * 0.165
+Weight on Mars = weight on Earth * 0.378
+Weight on Jupiter = weight on Earth * 2.528
+Notice:
+1. Please write pseudo-code before coding the function.
+2. Your function should include docstring.
+3. Write your own test code, i.e. call the function.
+"""
+
+def weight_2():
+    """
+    this function takes your weight on earth
+    then converts your earth weight to the weight of another planet"""
+    #1. ask the weight of on earth
+    #2. ask the which planet they are at
+    #3. return the the weight in the appropriate planet
+    weight = float(input('What is your weight on earth:'))
+    planet = str(input('what planet you are at: (plase make the first letter of the planet capital)'))
+    if planet == 'Moon':
+        return(weight * 0.165)
+    elif planet == 'Mars':
+        return(weight * 0.378)
+    elif planet == 'Jupiter':
+        return(weight * 2.528)
+    else:
+        print('Sorry, don\'t know that planet')
+
+print(weight_2())
