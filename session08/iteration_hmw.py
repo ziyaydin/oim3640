1cf576ac24ea15a0169b8ed9d600f5120ae0528f
ziyaydin <zaydin1@babson.edu>
2022-02-12 19:08:16 -0500

    Update iteration_hmw.py

--- session08/iteration_hmw.py
+++ session08/iteration_hmw.py
@@ -1,45 +1,47 @@
-#EX2.2
+# EX2.2
 
-#Calculate the sum of integers from 1 to 10. WITH WHILE LOOP
+# Calculate the sum of integers from 1 to 10. WITH WHILE LOOP
 def sum_from_0_to(ceiling_number):
     """ prints the sum of numbers from 0 to ceiling_number """
-    summ  = 0
+    summ = 0
     new_number = 1
     while new_number <= ceiling_number:
         summ = summ + new_number
         new_number = new_number + 1
     print(summ)
 
-    if ceiling_number * (ceiling_number  + 1) / 2 == summ:
+    if ceiling_number * (ceiling_number + 1) / 2 == summ:
         print("double check succesful")
-    else: 
+    else:
         print("something is wrong")
 
 # sum_from_0_to(10)
 
-#Calculate the sum of integers from 1 to 1000. (hint: we need to use range().) WITH WHILE LOOP
+# Calculate the sum of integers from 1 to 1000. (hint: we need to use range().) WITH WHILE LOOP
+
+
 def sum_from_0_to(ceiling_number):
     """ prints the sum of numbers from 0 to ceiling_number """
-    summ  = 0
+    summ = 0
     new_number = 1
     while new_number <= ceiling_number:
         summ = summ + new_number
         new_number = new_number + 1
     print(summ)
 
-    if ceiling_number * (ceiling_number  + 1) / 2 == summ:
+    if ceiling_number * (ceiling_number + 1) / 2 == summ:
         print("double check succesful")
-    else: 
+    else:
         print("something is wrong")
-    
+
 # sum_from_0_to(1000)
 
 
-#Calculate the sum of all the odd numbers from 1 to 1000. (hint: check out range() function in Python documentation.) How about all the even numbers? WITH WHILE LOOP
+# Calculate the sum of all the odd numbers from 1 to 1000. (hint: check out range() function in Python documentation.) How about all the even numbers? WITH WHILE LOOP
 
 def sum_of_odds_from_0_to(ceiling_number):
     """ prints the sum of numbers from 0 to ceiling_number """
-    summ  = 0
+    summ = 0
     new_number = 1
     while new_number <= ceiling_number:
         if new_number % 2 == 1:
@@ -48,7 +50,9 @@ def sum_of_odds_from_0_to(ceiling_number):
     print(summ)
 # sum_of_odds_from_0_to(100)
 
-#EX 3 TODO: Figure out how to print table. Tried tabulate function but didint work
+# EX 3 TODO: Figure out how to print table. Tried tabulate function but didint work
+
+
 def newtons_method(a):
     x = a/2
     while True:
@@ -57,5 +61,6 @@ def newtons_method(a):
             break
         x = y
     print(x)
+
 
 newtons_method(9)
