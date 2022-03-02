1c3cf2d64eeac6ff60edec9286efd1a06e20447b
ziyaydin <zaydin1@babson.edu>
2022-03-02 11:56:11 -0500

    Create lists.py

---
+++ session12/lists.py
@@ -0,0 +1,42 @@
+# numbers = [2, 0, 1, 6, 9]
+
+# for i in range(len(numbers)):
+#     numbers[i] = numbers[i] * 2
+    
+# print(numbers)
+
+# my_list = ['spam', 1, ['New England Patriots', \
+#                        'Buffalo Bills', 'Miami Dolphins', \
+#                        'New York Giants'], \
+#            [1, 2, 3]]
+# print(len(my_list))
+
+# a = [1, 2, 3]
+# b = [4, 5, 6]
+# c = a + b
+# print(c)
+# [0] * 4
+
+# z = ['Tom Brady', 'Bill Belichick'] * 3
+# print(z)
+
+# t = ['a', 'b', 'c', 'd', 'e', 'f']
+# t = ['a', 'b', 'c', 'd', 'e', 'f']
+# t[1:3] = ['x', 'y']
+# print(t)
+
+#EXERCISE 02
+# x = ['a', 'z', 'c']
+# x.append('a')
+# print(x)
+
+y = ['a', 'z', 'c']
+# y2 = ['a', 'z']
+# y.extend(y2)
+# print(y)
+
+# z = ['a', 'b', 'c']
+# z.pop('')
+# print(z)
+
+print(set(y))
