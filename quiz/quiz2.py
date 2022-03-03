7f4fad3f303684de2186df2dd3491caa03e14983
ziyaydin <zaydin1@babson.edu>
2022-03-03 16:05:13 -0500

    Create quiz2.py

---
+++ quiz/quiz2.py
@@ -0,0 +1,37 @@
+# #A bad word should:
+# - contain at least three letters from "covid",
+# - and contain at least one letter that occurs twice in a row,
+# - and have same first letter and last letter. 
+def uses_only(word, available):
+    """
+    takes a word and a string of letters, and that returns True if the word
+    contains only letters in the string available.
+    """
+    for letter in word: 
+        if letter not in available:
+            return False
+    return True
+
+def find_words_only_use_planet(): 
+    """This function creates words that only has the letters of "planet" 
+    """
+    f = open('data/words.txt')  # Assume words.txt is under data folder
+    words_with_planets= 0 
+    for word in f:
+        word = word.strip()
+        if uses_only(word,'planets'):
+            words_with_planets +=1
+    return words_with_planets
+
+def bad_word():
+    """ 
+    """
+    f = open('data/random_words.txt')  #we need this to look at randomword data file
+    bad = 0
+    for word in f: #USES LETTERS FROM COVID AT LEAST ONCE
+        if uses_only(word, 'covid'):
+            bad += 1
+            return bad
+        for letters in word:
+            if word[letters] == word[letters]: #WIP
+        pass
