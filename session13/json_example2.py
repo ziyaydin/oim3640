964c13c34a07faa9ccfa8cbb62e35eb59ef13936
ziyaydin <zaydin1@babson.edu>
2022-03-06 18:28:25 -0500

    Update json_example2.py

--- session13/json_example2.py
+++ session13/json_example2.py
@@ -2,3 +2,20 @@ weather = {'coord': {'lon': -71.29, 'lat': 42.3}, 'weather': [{'id': 500, 'main'
 
 
 # what is current temperature?
+
+import urllib.request
+import json
+
+APIKEY = '895c1b03bc0f9a1178c91126bb892dbf'
+city = 'Wellesley'
+country_code = 'us'
+url = f'http://api.openweathermap.org/data/2.5/weather?q={city},{country_code}&APPID={APIKEY}'
+
+# print(url)
+f = urllib.request.urlopen(url)
+response_text = f.read().decode('utf-8')
+response_data = json.loads(response_text)
+print(response_data['main']['temp'])
+
+# How do we get current temperature?
+
