59bd26d9424c8577eefa7ce705f17db201f83e9d
ziyaydin <zaydin1@babson.edu>
2022-03-05 23:34:38 -0500

    Update people_in_space.py

--- session13/people_in_space.py
+++ session13/people_in_space.py
@@ -1,5 +1,6 @@
 import urllib.request
 import json
+import pprint 
 
 url = "http://api.open-notify.org/astros.json"
 
@@ -10,7 +11,13 @@ with urllib.request.urlopen(url) as f:
     # print(type(j))
 
 # Can you print number of people in the space?
-print(j['people'])
+
+# print(j['number'])
 
 
 # Can you print all the names?
+
+# pprint.pprint(j['people'])
+
+for astro in j['people']:
+    print(astro['name'], 'is in', astro['craft'])
