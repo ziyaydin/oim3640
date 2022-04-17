a61d4bd3de5cc8b1e951890e7a265d282a7bfc06
ziyaydin <zaydin1@babson.edu>
2022-04-17 11:33:32 -0400

    exercise5

--- OOP/OOP3/exercise_5.py
+++ OOP/OOP3/exercise_5.py
@@ -12,5 +12,4 @@ class football_players:
     
     def __str__(self):
         return f'{self.name} plays for {self.team} and has scored {self.goals} and assisted {self.assists} times. But will play in {self.world_cups} world cup.'
-
     
